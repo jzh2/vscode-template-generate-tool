b6adb33b6146e3a392098b66de354e17f1d0801e
admin_jzh <hcxx6227jzh@gmail.com>
2023-09-06 15:04:43 +0800

    fix:页签用了const

--- extension.js
+++ extension.js
@@ -1,32 +1,32 @@
-const vscode = require("vscode");
-let generatePanel = null;
+const vscode = require('vscode')
+let generatePanel = null
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  const settingsDisposable = getDisposable("设置", "settings", context);
-  const formDisposable = getDisposable("表单生成", "form", context);
-  const tableDisposable = getDisposable("表格生成", "table", context);
-  const apiDisposable = getDisposable("API生成", "api", context);
+  const settingsDisposable = getDisposable('设置', 'settings', context)
+  const formDisposable = getDisposable('表单生成', 'form', context)
+  const tableDisposable = getDisposable('表格生成', 'table', context)
+  const apiDisposable = getDisposable('API生成', 'api', context)
 
-  const webDisposable = getWebDisposable(context);
+  const webDisposable = getWebDisposable(context)
 
   const docDisposable = getDocDisposable(
-    "前端文档",
-    "http://172.18.150.201:10012/frontend-docs/pc/dev-env",
+    '前端文档',
+    'http://172.18.150.201:10012/frontend-docs/pc/dev-env',
     context
-  );
+  )
   const componentDisposable = getDocDisposable(
-    "前端组件库",
-    "http://172.18.166.134:31034/frontend-component-doc/index.html#/changelog",
+    '前端组件库',
+    'http://172.18.166.134:31034/frontend-component-doc/index.html#/changelog',
     context
-  );
+  )
   const cssDisposable = getDocDisposable(
-    "样式工具类",
-    "http://172.18.150.201:10012/frontend-docs/pc/css-utils",
+    '样式工具类',
+    'http://172.18.150.201:10012/frontend-docs/pc/css-utils',
     context
-  );
+  )
 
   context.subscriptions.push(
     settingsDisposable,
@@ -37,7 +37,7 @@ function activate(context) {
     docDisposable,
     componentDisposable,
     cssDisposable
-  );
+  )
 }
 
 // 工具面板
@@ -46,108 +46,108 @@ function getDisposable(commandName, activeName, context) {
     `vscode-template-generate-tool.${commandName}`,
     () => {
       if (generatePanel) {
-        generatePanel.reveal(vscode.ViewColumn.Beside);
+        generatePanel.reveal(vscode.ViewColumn.Beside)
       } else {
         generatePanel = vscode.window.createWebviewPanel(
-          "template-generate-tool",
-          "模板代码生成工具",
+          'template-generate-tool',
+          '模板代码生成工具',
           vscode.ViewColumn.Beside,
           {
             enableScripts: true,
-            retainContextWhenHidden: true,
+            retainContextWhenHidden: true
           }
-        );
+        )
         generatePanel.webview.html = _getWebviewContent(
           generatePanel.webview,
           context.extensionUri
-        );
+        )
         generatePanel.webview.onDidReceiveMessage(
-          async (message) => {
+          async message => {
             switch (message.command) {
-              case "openWebsite":
-                const panel = vscode.window.createWebviewPanel(
-                  "template-generate-tool",
-                  "参考文档",
+              case 'openWebsite':
+                let panel = vscode.window.createWebviewPanel(
+                  'template-generate-tool',
+                  '参考文档',
                   vscode.ViewColumn.One,
                   {
                     enableScripts: true,
-                    retainContextWhenHidden: true,
+                    retainContextWhenHidden: true
                   }
-                );
-                panel.webview.html = loadDocument(message.website);
+                )
+                panel.webview.html = loadDocument(message.website)
                 panel.onDidDispose(
                   () => {
-                    panel = null;
+                    panel = null
                   },
                   null,
                   context.subscriptions
-                );
-                return;
-              case "openExternalWebsite":
-                vscode.env.openExternal(vscode.Uri.parse(message.website));
-                return;
-              case "openFolder":
+                )
+                return
+              case 'openExternalWebsite':
+                vscode.env.openExternal(vscode.Uri.parse(message.website))
+                return
+              case 'openFolder':
                 await vscode.commands.executeCommand(
-                  "vscode.openFolder",
+                  'vscode.openFolder',
                   vscode.Uri.file(
                     `${vscode.workspace.workspaceFolders[0].uri.path}\\${message.folder}`
                   ),
                   {
-                    forceNewWindow: true, // 新窗口打开
+                    forceNewWindow: true // 新窗口打开
                   }
-                );
-                return;
-              case "openFile":
+                )
+                return
+              case 'openFile':
                 await vscode.commands.executeCommand(
-                  "vscode.openFolder",
+                  'vscode.openFolder',
                   vscode.Uri.file(
                     `${vscode.workspace.workspaceFolders[0].uri.path}\\${message.file}`
                   )
-                );
-                return;
+                )
+                return
             }
           },
           undefined,
           context.subscriptions
-        );
+        )
         generatePanel.onDidDispose(
           () => {
-            generatePanel = null;
+            generatePanel = null
           },
           null,
           context.subscriptions
-        );
+        )
       }
-      generatePanel.webview.postMessage({ activeName });
+      generatePanel.webview.postMessage({ activeName })
     }
-  );
+  )
 }
 
 function _getWebviewContent(webview, extensionUri) {
   const scriptUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "js",
-    "index.js",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'js',
+    'index.js'
+  ])
   const chunkScriptUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "js",
-    "chunk-vendors.js",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'js',
+    'chunk-vendors.js'
+  ])
   const stylesUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "css",
-    "index.css",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'css',
+    'index.css'
+  ])
   const chunkStylesUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "css",
-    "chunk-vendors.css",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'css',
+    'chunk-vendors.css'
+  ])
   return /*html*/ `
 <!DOCTYPE html>
 <html lang="">
@@ -164,7 +164,7 @@ function _getWebviewContent(webview, extensionUri) {
     <div id="app"></div>
   </body>
 </html>
-`;
+`
 }
 
 // 浏览器面板
@@ -172,55 +172,55 @@ function getWebDisposable(context) {
   return vscode.commands.registerCommand(
     `vscode-template-generate-tool.新建浏览器页签`,
     () => {
-      const webPanel = vscode.window.createWebviewPanel(
-        "template-generate-tool",
-        "浏览器",
+      let webPanel = vscode.window.createWebviewPanel(
+        'template-generate-tool',
+        '浏览器',
         vscode.ViewColumn.One,
         {
           enableScripts: true,
-          retainContextWhenHidden: true,
+          retainContextWhenHidden: true
         }
-      );
+      )
       webPanel.webview.html = loadWebsite(
         webPanel.webview,
         context.extensionUri
-      );
+      )
       webPanel.onDidDispose(
         () => {
-          webPanel = null;
+          webPanel = null
         },
         null,
         context.subscriptions
-      );
+      )
     }
-  );
+  )
 }
 
 function loadWebsite(webview, extensionUri) {
   const scriptUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "js",
-    "star-website.js",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'js',
+    'star-website.js'
+  ])
   const chunkScriptUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "js",
-    "chunk-vendors.js",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'js',
+    'chunk-vendors.js'
+  ])
   const stylesUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "css",
-    "star-website.css",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'css',
+    'star-website.css'
+  ])
   const chunkStylesUri = getUri(webview, extensionUri, [
-    "template-generate-tool",
-    "dist",
-    "css",
-    "chunk-vendors.css",
-  ]);
+    'template-generate-tool',
+    'dist',
+    'css',
+    'chunk-vendors.css'
+  ])
   return /*html*/ `
 <!DOCTYPE html>
 <html lang="">
@@ -237,7 +237,7 @@ function loadWebsite(webview, extensionUri) {
     <div id="app"></div>
   </body>
 </html>
-`;
+`
 }
 
 // 文档面板
@@ -245,25 +245,25 @@ function getDocDisposable(commandName, url, context) {
   return vscode.commands.registerCommand(
     `vscode-template-generate-tool.${commandName}`,
     () => {
-      const docPanel = vscode.window.createWebviewPanel(
-        "template-generate-tool",
+      let docPanel = vscode.window.createWebviewPanel(
+        'template-generate-tool',
         commandName,
         vscode.ViewColumn.One,
         {
           enableScripts: true,
-          retainContextWhenHidden: true,
+          retainContextWhenHidden: true
         }
-      );
-      docPanel.webview.html = loadDocument(url);
+      )
+      docPanel.webview.html = loadDocument(url)
       docPanel.onDidDispose(
         () => {
-          docPanel = null;
+          docPanel = null
         },
         null,
         context.subscriptions
-      );
+      )
     }
-  );
+  )
 }
 
 function loadDocument(url) {
@@ -288,16 +288,16 @@ function loadDocument(url) {
       <iframe src="${url}"/>
     </body>
   </html>
-  `;
+  `
 }
 
 function getUri(webview, extensionUri, pathList) {
-  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
+  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList))
 }
 
 function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate,
-};
+  deactivate
+}
